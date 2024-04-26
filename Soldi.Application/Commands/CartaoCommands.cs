7b9bde29810bc35ad10b4dabc421f66601790be4
Jonas Souza <jonassouza@live.com>
2024-04-26 07:54:37 -0300

    Criados Handler de Cartão e commands

---
+++ Soldi.Application/Commands/CartaoCommands.cs
@@ -0,0 +1,16 @@
+﻿using Soldi.Core.Base;
+using Soldi.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soldi.Application.Commands
+{
+
+    public record CartaoAdicionarCommand(Guid usuarioId, string? nome, string? imagem, int diaFechamento, int diaVencimento, Guid contaPagamentoPadrao) : ICommand { }
+    public record  CartaoAtualizarCommand(Guid id, string? nome, string? imagem, int diaFechamento, int diaVencimento, Guid contaPagamentoPadrao) :ICommand { }
+
+
+}
---
+++ Soldi.Application/Handlers/Cartao/CartaoCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Soldi.Application.Commands;
+using Soldi.Application.Commands.Cartao;
+using Soldi.Application.DTO;
+using Soldi.Core.Base;
+using Soldi.Core.Entities;
+using Soldi.Core.Enums;
+
+
+namespace Soldi.Application.Handlers
+{
+    public class CartaoCommandHandler : ICommandHandler<CartaoAdicionarCommand>, ICommandHandler<CartaoAtualizarCommand>
+    {
+        private IMapper _mapper;
+        private IUnitOfWork _uow;
+
+        public CartaoCommandHandler(IMapper mapper, IUnitOfWork uow)
+        {
+            _mapper = mapper;
+            _uow = uow;
+        }
+
+
+
+        public async Task<(bool, string)> Handle(CartaoAdicionarCommand command)
+        {
+
+            var conta = new Cartao(
+                usuarioId: command.usuarioId,
+                contaPagamentoPadrao:command.contaPagamentoPadrao,
+                diaFechamento:command.diaFechamento,
+                diaVencimento:command.diaVencimento,
+                nome: command.nome,
+                imagem: command.imagem);
+
+            var result = conta.Validar();
+            if (result.status == false) return result;
+
+
+            _uow.CartaoRepository.Create(conta);
+            return await _uow.Commit();
+
+        }
+
+        public async Task<(bool Success, string Message)> Handle(CartaoAtualizarCommand command)
+        {
+            var conta = await _uow.CartaoRepository.GetByIdAsync(command.id);
+            conta.Atualizar(
+                contaPagamentoPadrao: command.contaPagamentoPadrao,
+                diaFechamento: command.diaFechamento,
+                diaVencimento: command.diaVencimento,
+                nome: command.nome,
+                imagem: command.imagem
+                );
+
+            var result = conta.Validar();
+            if (result.status == false) return result;
+
+
+            _uow.CartaoRepository.Update(conta);
+            return await _uow.Commit();
+        }
+
+       
+    }
+}
