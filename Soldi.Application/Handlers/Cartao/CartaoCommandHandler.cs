d193a096a6313105b2c3b2d9d0a434307d248ec4
Jonas Souza <jonassouza@live.com>
2024-05-05 20:38:57 -0300

    Adicionado rastreamento de erros e retorno de falhas nos handlers de Cartao e Conta

--- Soldi.Application/Handlers/Cartao/CartaoCommandHandler.cs
+++ Soldi.Application/Handlers/Cartao/CartaoCommandHandler.cs
@@ -21,47 +21,65 @@ namespace Soldi.Application.Handlers
 
         public async Task<(bool, string)> Handle(CartaoAdicionarCommand command)
         {
-
-            var conta = new Cartao(
-                usuarioId: command.usuarioId,
-                contaPagamentoPadrao:command.contaPagamentoPadrao,
-                diaFechamento:command.diaFechamento,
-                diaVencimento:command.diaVencimento,
-                nome: command.nome,
-                imagem: command.imagem);
-
-            var result = conta.Validar();
-            if (result.status == false) return result;
-
-
-            _uow.CartaoRepository.Create(conta);
-            return await _uow.Commit();
-
-        }
-
-        public async Task<(bool Success, string Message)> Handle(CartaoAtualizarCommand command)
-        {
-            var conta = await _uow.CartaoRepository.GetByIdAsync(command.id);
-            if (conta!=null)
+            try
             {
-                conta.Atualizar(
-                              contaPagamentoPadrao: command.contaPagamentoPadrao,
-                              diaFechamento: command.diaFechamento,
-                              diaVencimento: command.diaVencimento,
-                              nome: command.nome,
-                              imagem: command.imagem
-                              );
+
+                var conta = new Cartao(
+                    usuarioId: command.usuarioId,
+                    contaPagamentoPadrao: command.contaPagamentoPadrao,
+                    diaFechamento: command.diaFechamento,
+                    diaVencimento: command.diaVencimento,
+                    nome: command.nome,
+                    imagem: command.imagem);
 
                 var result = conta.Validar();
                 if (result.status == false) return result;
 
 
-                _uow.CartaoRepository.Update(conta);
+                _uow.CartaoRepository.Create(conta);
                 return await _uow.Commit();
             }
-            return (false, "Não encontrado!");
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
+
         }
 
-       
+        public async Task<(bool Success, string Message)> Handle(CartaoAtualizarCommand command)
+        {
+            try
+            {
+                var conta = await _uow.CartaoRepository.GetByIdAsync(command.id);
+                if (conta != null)
+                {
+                    conta.Atualizar(
+                                  contaPagamentoPadrao: command.contaPagamentoPadrao,
+                                  diaFechamento: command.diaFechamento,
+                                  diaVencimento: command.diaVencimento,
+                                  nome: command.nome,
+                                  imagem: command.imagem
+                                  );
+
+                    var result = conta.Validar();
+                    if (result.status == false) return result;
+
+
+                    _uow.CartaoRepository.Update(conta);
+                    return await _uow.Commit();
+                }
+                return (false, "Não encontrado!");
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
+        }
+
+
     }
 }
--- Soldi.Application/Handlers/Conta/ContaCommandHandler.cs
+++ Soldi.Application/Handlers/Conta/ContaCommandHandler.cs
@@ -5,6 +5,7 @@ using Soldi.Application.DTO;
 using Soldi.Core.Base;
 using Soldi.Core.Entities;
 using Soldi.Core.Enums;
+using System.Linq.Expressions;
 
 
 namespace Soldi.Application.Handlers
@@ -24,75 +25,120 @@ namespace Soldi.Application.Handlers
 
         public async Task<(bool, string)> Handle(ContaAdicionarCommand command)
         {
+            try
+            {
+                var conta = new Conta(
+                            usuarioId: command.usuarioId,
+                            nome: command.nome,
+                            saldo: command.saldo,
+                            imagem: command.imagem,
+                            tipoConta: (ETipoConta)command.tipoConta);
 
-            var conta = new Conta(
-                usuarioId: command.usuarioId,
-                nome: command.nome,
-                saldo: command.saldo,
-                imagem: command.imagem,
-                tipoConta: (ETipoConta)command.tipoConta);
-
-            var result = conta.Validar();
-            if (result.status == false) return result;
+                var result = conta.Validar();
+                if (result.status == false) return result;
 
 
-            _uow.ContaRepository.Create(conta);
-            return await _uow.Commit();
+                _uow.ContaRepository.Create(conta);
+                return await _uow.Commit();
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
+
 
         }
 
         public async Task<(bool Success, string Message)> Handle(ContaAtualizarCommand command)
         {
-            var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
-            conta.Atualizar(
-                nome: command.nome,
-                imagem: command.imagem,
-                tipoConta: (ETipoConta)command.tipoConta);
+            try
+            {
+                var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
+                conta.Atualizar(
+                    nome: command.nome,
+                    imagem: command.imagem,
+                    tipoConta: (ETipoConta)command.tipoConta);
 
-            var result = conta.Validar();
-            if (result.status == false) return result;
+                var result = conta.Validar();
+                if (result.status == false) return result;
 
 
-            _uow.ContaRepository.Update(conta);
-            return await _uow.Commit();
+                _uow.ContaRepository.Update(conta);
+                return await _uow.Commit();
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
         }
 
         public async Task<(bool Success, string Message)> Handle(ContaRetiradaCommand command)
         {
-            var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
-            conta.RetiradaSaldo(saldo:command.saldo);
+            try
+            {
+                var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
+                conta.RetiradaSaldo(saldo: command.saldo);
 
-            var result = conta.Validar();
-            if (result.status == false) return result;
+                var result = conta.Validar();
+                if (result.status == false) return result;
 
 
-            _uow.ContaRepository.Update(conta);
-            return await _uow.Commit();
+                _uow.ContaRepository.Update(conta);
+                return await _uow.Commit();
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
         }
 
         public async Task<(bool Success, string Message)> Handle(ContaDepositoCommand command)
         {
-            var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
-            conta.DepositoSaldo(saldo: command.saldo);
+            try
+            {
+                var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
+                conta.DepositoSaldo(saldo: command.saldo);
 
-            var result = conta.Validar();
-            if (result.status == false) return result;
+                var result = conta.Validar();
+                if (result.status == false) return result;
 
 
-            _uow.ContaRepository.Update(conta);
-            return await _uow.Commit();
+                _uow.ContaRepository.Update(conta);
+                return await _uow.Commit();
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
         }
         public async Task<(bool Success, string Message)> Handle(ContaDefinirSaldoCommand command)
         {
-            var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
-            conta.DefinirSaldo(saldo: command.saldo);
+            try
+            {
+                var conta = await _uow.ContaRepository.GetByIdAsync(command.id);
+                conta.DefinirSaldo(saldo: command.saldo);
 
-            var result = conta.Validar();
-            if (result.status == false) return result;
+                var result = conta.Validar();
+                if (result.status == false) return result;
 
 
-            _uow.ContaRepository.Update(conta);
-            return await _uow.Commit();
+                _uow.ContaRepository.Update(conta);
+                return await _uow.Commit();
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message + "\n\n" + ex.StackTrace);
+            }
+
         }
     }
 }
